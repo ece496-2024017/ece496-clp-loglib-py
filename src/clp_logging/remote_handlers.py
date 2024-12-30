a85a12bc06d63feab80b5a36d7a35b9eab30b351
IreneLi <irene2002yr@gmail.com>
2024-12-30 15:59:28 -0500

    Reformat code for formatting alignment

--- src/clp_logging/remote_handlers.py
+++ src/clp_logging/remote_handlers.py
@@ -1,6 +1,8 @@
 import boto3
 from botocore.exceptions import NoCredentialsError
-from botocore.exceptions import ClientError
+# from botocore.exceptions import ClientError
+from typing import Callable, ClassVar, Dict, IO, Optional, Tuple, Union, List, Any
+from pathlib import Path
 import datetime
 import hashlib
 import os
@@ -13,34 +15,37 @@ class CLPRemoteHandler():
     Configuration of AWS access key is required. Run command `aws configure`
     """
 
-    def __init__(self, log_name, log_path, s3_bucket) -> None:
-        self.s3_resource = boto3.resource("s3")
-        self.s3_client = boto3.client("s3")
-        self.log_path = log_path
-        self.log_name = log_name
-        self.transfer = 0
-        self.bucket = s3_bucket
-        self.remote_file_count = 0
+    def __init__(
+            self,
+            log_name: str,
+            log_path: Path,
+            s3_bucket: str,
+    ) -> None:
+        self.s3_resource: boto3.resources.factory.s3.ServiceResource = boto3.resource("s3")
+        self.s3_client: boto3.client = boto3.client("s3")
+        self.log_name: str = log_name
+        self.log_path: Path = log_path
+        self.bucket: str = s3_bucket
+        self.remote_file_count: int = 0
 
-        self.timestamp = datetime.datetime.now()
-        self.folder_path = f"logs/{self.timestamp.year}/{self.timestamp.month}/{self.timestamp.day}"
+        self.timestamp: datetime.datetime = datetime.datetime.now()
+        self.folder_path: str = f"logs/{self.timestamp.year}/{self.timestamp.month}/{self.timestamp.day}"
 
         # print(f"File name: {self.log_name}; File path: {self.log_path}")
-        self.obj_key = self._remote_log_naming()
+        self.obj_key: str = self._remote_log_naming()
         print(self.obj_key)
 
-
-
-        self.multipart_upload_config = {
+        self.multipart_upload_config: Dict[str, int] = {
             "size": 1024 * 1024 * 5,
             "index": 1,
             "pos": 0,
-            "uploaded parts": [],
         }
+        self.uploaded_parts: List[Dict[str, int | str]] = []
 
-    def _remote_log_naming(self):
-        ext = self.log_name.find(".")
-        upload_time = self.timestamp.strftime("%Y-%m-%d-%H%M%S")
+    def _remote_log_naming(self) -> str:
+        new_filename: str
+        ext: int = self.log_name.find(".")
+        upload_time: str = self.timestamp.strftime("%Y-%m-%d-%H%M%S")
         if self.remote_file_count != 0:
             upload_time += "-" + str(self.remote_file_count)
 
@@ -48,35 +53,33 @@ class CLPRemoteHandler():
             new_filename = f'log_{upload_time}{self.log_name[ext:]}'
         else:
             new_filename = f'{upload_time}_{self.log_name}'
-        file_name = f"{self.folder_path}/{new_filename}"
-        return file_name
+        new_filename = f"{self.folder_path}/{new_filename}"
+        return new_filename
 
-    def _calculate_part_sha256(self, data):
+    def _calculate_part_sha256(self, data) -> str:
         """Calculate the SHA256 checksum for the provided data."""
-        sha256_hash = hashlib.sha256()
+        sha256_hash: hashlib.Hash = hashlib.sha256()
         sha256_hash.update(data)
         return base64.b64encode(sha256_hash.digest()).decode('utf-8')
 
 
-    def _upload_part(self, upload_id):
+    def _upload_part(self, upload_id) -> Dict[str, int | str]:
+        upload_data: bytes
         # Read the latest file
         try:
             with open(self.log_path, 'rb') as file:
                 file.seek(self.multipart_upload_config["pos"])
                 upload_data = file.read(self.multipart_upload_config["size"])
         except FileNotFoundError as e:
-            print(f"The log file {self.log_path} cannot be found: {e}")
-            raise
+            raise FileNotFoundError(f"The log file {self.log_path} cannot be found: {e}") from e
         except IOError as e:
-            print(f"IO Error occurred while reading file {self.log_path}: {e}")
-            raise
+            raise IOError(f"IO Error occurred while reading file {self.log_path}: {e}") from e
         except Exception as e:
-            print(f"Exception occurred: {e}")
-            raise
+            raise e
 
         try:
-            sha256_checksum = self._calculate_part_sha256(upload_data)
-            response = self.s3_client.upload_part(
+            sha256_checksum: str = self._calculate_part_sha256(upload_data)
+            response: Dict[str, Any] = self.s3_client.upload_part(
                 Bucket=self.bucket,
                 Key=self.obj_key,
                 Body=upload_data,
@@ -93,47 +96,54 @@ class CLPRemoteHandler():
                 "ETag": response["ETag"],
                 "ChecksumSHA256": response["ChecksumSHA256"],
             }
-        except ClientError as e:
-            print(f"ClientError occurred during multipart upload on {self.multipart_upload_config['index']}: {e}")
-            raise
         except Exception as e:
-            print(f"Exception occurred: {e}")
-            raise
+            self.s3_client.abort_multipart_upload(
+                Bucket=self.bucket, Key=self.obj_key, UploadId=upload_id
+            )
+            raise Exception(f"Error occurred during multipart upload on part {self.multipart_upload_config['index']}: {e}") from e
 
-    def _complete_upload(self, upload_id):
-        return self.s3_client.complete_multipart_upload(
+    def _complete_upload(self, upload_id) -> None:
+        response = self.s3_client.complete_multipart_upload(
             Bucket=self.bucket,
             Key=self.obj_key,
             UploadId=upload_id,
             MultipartUpload={
                 "Parts": [
                     {"PartNumber": part["PartNumber"], "ETag": part["ETag"], "ChecksumSHA256": part["ChecksumSHA256"]}
-                    for part in self.multipart_upload_config["uploaded parts"]
+                    for part in self.uploaded_parts
                 ]
             },
         )
 
-    def get_obj_key(self):
+        print(response)
+        print("Complete multipart upload")
+        try:
+            response = self.s3_client.head_object(Bucket=self.bucket, Key=self.obj_key)
+            print("Object metadata:", response)
+        except Exception as e:
+            print("Object not found:", e)
+
+    def get_obj_key(self) -> str:
         return self.obj_key
 
-    def set_obj_key(self, obj_key):
+    def set_obj_key(self, obj_key) -> None:
         self.obj_key = obj_key
 
-    def multipart_upload(self):
+    def multipart_upload(self) -> None:
         print(f"Initiate Multipart Upload of file {self.log_name}")
-        create_ret = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key, ChecksumAlgorithm="SHA256")
-        upload_id = create_ret["UploadId"]
-        file_size = os.path.getsize(self.log_path)
+        create_ret: Dict[str, Any] = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key, ChecksumAlgorithm="SHA256")
+        upload_id: int = create_ret["UploadId"]
+        file_size: int = os.path.getsize(self.log_path)
         try:
             while (
                 file_size - self.multipart_upload_config["pos"]
                 >= self.multipart_upload_config["size"]
             ):
-                upload_status = self._upload_part(upload_id)
+                upload_status: Dict[str, int | str] = self._upload_part(upload_id)
                 print(upload_status)
                 self.multipart_upload_config["index"] += 1
                 self.multipart_upload_config["pos"] += self.multipart_upload_config["size"]
-                self.multipart_upload_config["uploaded parts"].append(upload_status)
+                self.uploaded_parts.append(upload_status)
 
                 # AWS S3 limits object part count to 10000
                 if self.multipart_upload_config["index"] > 10000:
@@ -143,7 +153,7 @@ class CLPRemoteHandler():
                     self.remote_file_count += 1
                     self.obj_key = self._remote_log_naming()
                     self.multipart_upload_config["index"] = 1
-                    self.multipart_upload_config["uploaded parts"] = []
+                    self.uploaded_parts = []
                     create_ret = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key,
                                                                         ChecksumAlgorithm="SHA256")
                     upload_id = create_ret["UploadId"]
@@ -152,23 +162,16 @@ class CLPRemoteHandler():
             # Upload the remaining segment
             if file_size - self.multipart_upload_config["pos"] < self.multipart_upload_config["size"]:
                 self.multipart_upload_config["size"] = file_size - self.multipart_upload_config["pos"]
-                response = self._upload_part(upload_id)
+                upload_status: Dict[str, int | str] = self._upload_part(upload_id)
                 self.multipart_upload_config["index"] += 1
-                self.multipart_upload_config["uploaded parts"].append(response)
+                self.uploaded_parts.append(upload_status)
 
-            response = self._complete_upload(upload_id)
-            print(response)
-            print("Complete multipart upload")
-            try:
-                response = self.s3_client.head_object(Bucket=self.bucket, Key=self.obj_key)
-                print("Object metadata:", response)
-            except Exception as e:
-                print("Object not found:", e)
+            self._complete_upload(upload_id)
 
-            return response
-        except NoCredentialsError as no_cred_error:
-            print(no_cred_error)
+        except NoCredentialsError as e:
+            raise e
         except Exception as e:
             self.s3_client.abort_multipart_upload(
                 Bucket=self.bucket, Key=self.obj_key, UploadId=upload_id
             )
+            raise e
