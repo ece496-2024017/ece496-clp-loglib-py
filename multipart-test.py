5033d085a86636309cdc745bc3ae1976b568f022
IreneLi <irene2002yr@gmail.com>
2025-01-01 22:16:28 -0500

    Separate the log name and log path from remotehandler instantiation requirement

--- multipart-test.py
+++ multipart-test.py
@@ -3,20 +3,17 @@ from src.clp_logging.remote_handlers import CLPRemoteHandler
 s3_bucket = "ictrl-test2024"
 
 # File around 270KB
-# log_name = "ictrl_2024-11-22-17-05.clp.zst"
-# log_path = "logs\ictrl_2024-11-22-17-05.clp.zst"
+# log_name2 = "ictrl_2024-11-22-17-05.clp.zst"
+# log_path2 = "logs\ictrl_2024-11-22-17-05.clp.zst"
 
 # File around 45MB
 log_name = "max-20.1.1.720.qdz"
 # log_path = "logs\multipart-example.clp.zst"
 log_path = "max-20.1.1.720.qdz"
 # Create a CLP Remote Handler instance
-remote_handler = CLPRemoteHandler(log_name, Path(log_path), s3_bucket)
-# print(remote_handler.compare_files())
-# Check if the file already exists on the cloud
-# if not remote_handler.compare_files():
-#     print("Cancel Upload.")
-# else:
-#     print()
-    # Upload the file if it does not exist on the cloud
+remote_handler = CLPRemoteHandler(s3_bucket)
+remote_handler.initiate_upload(log_name, Path(log_path))
+# remote_handler.initiate_upload(log_name2, Path(log_path2))
+
 remote_handler.multipart_upload()
+remote_handler.complete_upload()
--- src/clp_logging/remote_handlers.py
+++ src/clp_logging/remote_handlers.py
@@ -1,6 +1,6 @@
 import boto3
 from botocore.exceptions import NoCredentialsError
-# from botocore.exceptions import ClientError
+from clp_logging.handlers import *
 from typing import Callable, ClassVar, Dict, IO, Optional, Tuple, Union, List, Any
 from pathlib import Path
 import datetime
@@ -17,23 +17,19 @@ class CLPRemoteHandler():
 
     def __init__(
             self,
-            log_name: str,
-            log_path: Path,
             s3_bucket: str,
     ) -> None:
+        # super().__init__()
         self.s3_resource: boto3.resources.factory.s3.ServiceResource = boto3.resource("s3")
         self.s3_client: boto3.client = boto3.client("s3")
-        self.log_name: str = log_name
-        self.log_path: Path = log_path
+
         self.bucket: str = s3_bucket
         self.remote_file_count: int = 0
 
-        self.timestamp: datetime.datetime = datetime.datetime.now()
-        self.folder_path: str = f"logs/{self.timestamp.year}/{self.timestamp.month}/{self.timestamp.day}"
-
-        # print(f"File name: {self.log_name}; File path: {self.log_path}")
-        self.obj_key: str = self._remote_log_naming()
-        print(self.obj_key)
+        self.log_name: Optional[str] = None
+        self.log_path: Optional[Path] = None
+        self.remote_folder_path: Optional[str] = None
+        self.obj_key: Optional[str] = None
 
         self.multipart_upload_config: Dict[str, int] = {
             "size": 1024 * 1024 * 5,
@@ -41,11 +37,14 @@ class CLPRemoteHandler():
             "pos": 0,
         }
         self.uploaded_parts: List[Dict[str, int | str]] = []
+        self.upload_id: Optional[int] = None
+        self.upload_in_progress: bool = False
+        self.closed: bool = False
 
-    def _remote_log_naming(self) -> str:
+    def _remote_log_naming(self, timestamp: datetime.datetime) -> str:
         new_filename: str
         ext: int = self.log_name.find(".")
-        upload_time: str = self.timestamp.strftime("%Y-%m-%d-%H%M%S")
+        upload_time: str = timestamp.strftime("%Y-%m-%d-%H%M%S")
         if self.remote_file_count != 0:
             upload_time += "-" + str(self.remote_file_count)
 
@@ -53,14 +52,15 @@ class CLPRemoteHandler():
             new_filename = f'log_{upload_time}{self.log_name[ext:]}'
         else:
             new_filename = f'{upload_time}_{self.log_name}'
-        new_filename = f"{self.folder_path}/{new_filename}"
+        new_filename = f"{self.remote_folder_path}/{new_filename}"
         return new_filename
 
-    def _calculate_part_sha256(self, data) -> str:
+    def _calculate_part_sha256(self, data: bytes) -> str:
         """Calculate the SHA256 checksum for the provided data."""
         sha256_hash: hashlib.Hash = hashlib.sha256()
         sha256_hash.update(data)
         return base64.b64encode(sha256_hash.digest()).decode('utf-8')
+
 
 
     def _upload_part(self, upload_id) -> Dict[str, int | str]:
@@ -102,11 +102,49 @@ class CLPRemoteHandler():
             )
             raise Exception(f"Error occurred during multipart upload on part {self.multipart_upload_config['index']}: {e}") from e
 
-    def _complete_upload(self, upload_id) -> None:
+    def get_obj_key(self) -> str:
+        return self.obj_key
+
+    def set_obj_key(self, obj_key) -> None:
+        self.obj_key = obj_key
+
+    def initiate_upload(self, log_name: str, log_path: Path) -> None:
+        if self.upload_in_progress:
+            raise Exception("An upload is already in progress. Cannot initiate another upload.")
+
+        self.log_name: str = log_name
+        self.log_path: Path = log_path
+        self.upload_in_progress = True
+        timestamp: datetime.datetime = datetime.datetime.now()
+        self.remote_folder_path: str = f"logs/{timestamp.year}/{timestamp.month}/{timestamp.day}"
+
+        self.obj_key: str = self._remote_log_naming(timestamp)
+        create_ret: Dict[str, Any] = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key,
+                                                                            ChecksumAlgorithm="SHA256")
+        self.upload_id = create_ret["UploadId"]
+
+    def complete_upload(self) -> None:
+        if not self.upload_id:
+            raise Exception ("No upload process to complete.")
+
+        file_size: int = self.log_path.stat().st_size
+        try:
+            # Upload the remaining segment
+            if file_size - self.multipart_upload_config["pos"] < self.multipart_upload_config["size"]:
+                self.multipart_upload_config["size"] = file_size - self.multipart_upload_config["pos"]
+                upload_status: Dict[str, int | str] = self._upload_part(self.upload_id)
+                self.multipart_upload_config["index"] += 1
+                self.uploaded_parts.append(upload_status)
+        except Exception as e:
+            self.s3_client.abort_multipart_upload(
+                Bucket=self.bucket, Key=self.obj_key, UploadId=self.upload_id
+            )
+            raise e
+
         response = self.s3_client.complete_multipart_upload(
             Bucket=self.bucket,
             Key=self.obj_key,
-            UploadId=upload_id,
+            UploadId=self.upload_id,
             MultipartUpload={
                 "Parts": [
                     {"PartNumber": part["PartNumber"], "ETag": part["ETag"], "ChecksumSHA256": part["ChecksumSHA256"]}
@@ -114,7 +152,6 @@ class CLPRemoteHandler():
                 ]
             },
         )
-
         print(response)
         print("Complete multipart upload")
         try:
@@ -122,24 +159,22 @@ class CLPRemoteHandler():
             print("Object metadata:", response)
         except Exception as e:
             print("Object not found:", e)
-
-    def get_obj_key(self) -> str:
-        return self.obj_key
-
-    def set_obj_key(self, obj_key) -> None:
-        self.obj_key = obj_key
+        self.upload_in_progress = False
+        self.upload_id = None
+        self.obj_key = None
 
     def multipart_upload(self) -> None:
-        print(f"Initiate Multipart Upload of file {self.log_name}")
-        create_ret: Dict[str, Any] = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key, ChecksumAlgorithm="SHA256")
-        upload_id: int = create_ret["UploadId"]
-        file_size: int = os.path.getsize(self.log_path)
+        # Upload initiation is required before upload
+        if not self.upload_id:
+            raise Exception("No upload process.")
+
+        file_size: int = self.log_path.stat().st_size
         try:
             while (
                 file_size - self.multipart_upload_config["pos"]
                 >= self.multipart_upload_config["size"]
             ):
-                upload_status: Dict[str, int | str] = self._upload_part(upload_id)
+                upload_status: Dict[str, int | str] = self._upload_part(self.upload_id)
                 print(upload_status)
                 self.multipart_upload_config["index"] += 1
                 self.multipart_upload_config["pos"] += self.multipart_upload_config["size"]
@@ -147,7 +182,7 @@ class CLPRemoteHandler():
 
                 # AWS S3 limits object part count to 10000
                 if self.multipart_upload_config["index"] > 10000:
-                    self._complete_upload(upload_id)
+                    self.complete_upload(self.upload_id)
 
                     # Initiate multipart upload to a new S3 object
                     self.remote_file_count += 1
@@ -155,23 +190,13 @@ class CLPRemoteHandler():
                     self.multipart_upload_config["index"] = 1
                     self.uploaded_parts = []
                     create_ret = self.s3_client.create_multipart_upload(Bucket=self.bucket, Key=self.obj_key,
-                                                                        ChecksumAlgorithm="SHA256")
-                    upload_id = create_ret["UploadId"]
-
-
-            # Upload the remaining segment
-            if file_size - self.multipart_upload_config["pos"] < self.multipart_upload_config["size"]:
-                self.multipart_upload_config["size"] = file_size - self.multipart_upload_config["pos"]
-                upload_status: Dict[str, int | str] = self._upload_part(upload_id)
-                self.multipart_upload_config["index"] += 1
-                self.uploaded_parts.append(upload_status)
-
-            self._complete_upload(upload_id)
+                                                                                        ChecksumAlgorithm="SHA256")
+                    self.upload_id = create_ret["UploadId"]
 
         except NoCredentialsError as e:
             raise e
         except Exception as e:
             self.s3_client.abort_multipart_upload(
-                Bucket=self.bucket, Key=self.obj_key, UploadId=upload_id
+                Bucket=self.bucket, Key=self.obj_key, UploadId=self.upload_id
             )
             raise e
